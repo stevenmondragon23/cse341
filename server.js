33b706a08ffd722d45d352beb06f7e3cfd033c2e
stevenmondragon23 <smondragonflores@byupathway.edu>
2026-05-13 23:08:06 -0500

    connect mongo and vscode

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-require('dotenv').config();
-
+const mongodb = require('./data/database');
 const express = require('express');
 const app = express();
 
@@ -7,5 +6,12 @@ const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-
+mongodb.initDb((err) => {
+    if(err){
+        console.log(err);
+    }
+    else{
+        app.listen(port, () => {console.log(`Database is listening and mode running on port ${port}`)})
+    }
+});
 app.listen(port, () => {console.log(`Running on port ${port}`)});
